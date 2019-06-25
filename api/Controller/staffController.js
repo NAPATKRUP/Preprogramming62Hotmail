0fea4399c702e819708deefe5f119eb2c78b83ef
Tetat <tetat.chulchue@gmail.com>
2019-06-25 23:30:49 +0700

    Backend update
    + add firebase-admin
    + staff structure
    * add staff function in progress
    * todo : post staff data to database

---
+++ api/Controller/staffController.js
@@ -0,0 +1,7 @@
+import "../Entiy/Staff"
+const firebase = require('firebase-functions');
+const admin = require("firebase-admin");
+
+exports.addStaff = firebase.https.onRequest((request, response) => {
+    
+});
---
+++ api/Entiy/Staff.js
@@ -0,0 +1,6 @@
+export default function Staff(id, name, tatd, room) {
+    this.id = id
+    this.name = name;
+    this.tatd = tatd;
+    this.room = room;
+};
