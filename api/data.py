6e1424d42874f82c1a68e618a48d169354288d44
Tetat Chulchue <tetat.chulchue@gmail.com>
2019-06-27 15:30:40 +0700

    Update Python Script

--- api/data.py
+++ api/data.py
@@ -1,17 +1,35 @@
+""" Import Data To Firebase Using pyrebase4 """
+
 import pyrebase
 
 config = {
-  "apiKey": "AIzaSyDX-79Q7RsqKRqQC3dQQRTv60DRLW7pucA",
-  "authDomain": "preprogramming62hotmail.firebaseapp.com",
-  "databaseURL": "https://preprogramming62hotmail.firebaseio.com",
-  "storageBucket": ""
+    "apiKey": "AIzaSyDX-79Q7RsqKRqQC3dQQRTv60DRLW7pucA",
+    "authDomain": "preprogramming62hotmail.firebaseapp.com",
+    "databaseURL": "https://preprogramming62hotmail.firebaseio.com",
+    "storageBucket": ""
 }
 
 firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 ##################################################################
 
-db = firebase.database()
-data = {"name": "Mortimer 'Morty' Smith"}
-db.child("users").child("Morty").set(data)
+
+room = ["room-a", "room-b", "roomb-c", "room-c", "room-d", "room-e"]
+
+for current_room in room:
+    staff_id = place_holder
+    payload = {
+        "nickname": "nickname",
+        "state": "TD",
+        "mails": []
+    }
+    if payload:
+        db.child(current_room).child(staff_id).set(payload)
+
+
+
+
+# data = {"name": "Mortimer 'Morty' Smith"}
+# db.child("users").child("Morty").set(data)
 
