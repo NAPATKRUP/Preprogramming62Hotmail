eaaad4c2b3bef430e9122411be84338a96ce6ffb
Napat Arayawattanapong <napatkrup@hotmail.com>
2019-07-03 19:59:41 +0700

    Add view mails

--- src/components/DataCard.vue
+++ src/components/DataCard.vue
@@ -4,15 +4,27 @@
       <h3 class="myname">พี่{{ showname }}</h3>
       <img class="w-100 my-1" :src="showimage">
       <b-button v-b-modal.modal-1 @click="openmail()" class="tomail my-1">Send Mail</b-button>
-      <button class="showmail m-1"><img src="../assets/find.png" width="30px" height="30px"/></button>
+      <b-button v-b-modal.modal-2 @click="loginmail()" class="showmail m-1"><img src="../assets/find.png" width="30px" height="30px"/></b-button>
 
       <b-modal :id="modal-1" title="Send mail" :ref="select" @ok="pushmail">
         <p class="my-4">To พี่{{ showname }}</p>
         <div class="form-group shadow-textarea">
           <label>Form</label>
-          <textarea class="form-control z-depth-1" rows="1" placeholder="Your name..." v-model="name"> </textarea><br>
+          <textarea class="form-control z-depth-1" rows="1" placeholder="Your name..." v-model="name"></textarea><br>
           <label>Comment</label>
           <textarea class="form-control z-depth-1" rows="3" placeholder="Write something here..." v-model="mail"></textarea>
+        </div>
+      </b-modal>
+
+      <b-modal :id="modal-2" size="xl" title="Log in" :ref="showmailid" ok-only no-stacking @ok="clearpass">
+        <h2 class="my-4">พี่{{ showname }}</h2>
+        <div class="form-group shadow-textarea" v-if="this.getyourpass != this.correctpass">
+          <label>Password</label>
+          <textarea class="form-control z-depth-1" rows="1" placeholder="Your pass..." v-model="password"></textarea><br>
+          <button @click="login">Login</button>
+        </div>
+        <div v-if="this.getyourpass == this.correctpass">
+          <mail-card v-for="i in mymail" :maillist="i" :room="myclass" :id="select" v-bind:key="i"/>
         </div>
       </b-modal>
     </div>
@@ -23,29 +35,43 @@
 import { database } from 'firebase'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import MailCard from '@/components/MailCard'
 
 Vue.use(BootstrapVue)
 
 export default {
   name: 'DataCard',
+  components: {
+    MailCard,
+  },
   props: ['select', 'myclass'],
   data () {
     return {
       value: null,
       showname: null,
       showimage: null,
+      mymail: null,
+      getyourpass: null,
+      correctpass: null,
+      showmailid: this.select + "mail",
       name: "",
       mail: "",
     }
   },
 
   mounted () {
-    let data = database().ref(this.myclass + "/" + this.select)
     let self = this
+    let data = database().ref(this.myclass + "/" + this.select)
     data.on('value', function(snapshot) {
         self.value = snapshot.val()
         self.showname = self.value.nickname
         self.showimage = self.value.image
+        self.mymail = self.value.mails
+    })
+
+    let getpass = database().ref("password")
+    getpass.on('value', function(snapshot) {
+        self.correctpass = snapshot.val()
     })
   },
 
@@ -62,7 +88,20 @@ export default {
       else {
         alert("Please check your name and comment to try again")
       }
+      this.name = ""
+      this.mail = ""
     },
+    loginmail() {
+      var self = this
+      this.$refs[self.showmailid].show()
+    },
+    login() {
+      this.getyourpass = this.password
+      this.password = ""
+    },
+    clearpass() {
+      this.getyourpass = ""
+    }
   }
 }
 </script>
---
+++ src/components/MailCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-12 mymailshow my-3" v-if="value[0] != 'Sample Mails'">
+    <h5>{{ name }}</h5>
+    <hr>
+    <p>{{ mail }}</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+export default {
+  name: 'MailCard',
+  props: ['maillist', 'room', 'id'],
+  data () {
+    return {
+      value: null,
+      name: "noname",
+      mail: "nocomment"
+    }
+  },
+
+  mounted () {
+      this.value = this.maillist.split("-||-")
+      this.name = this.value[0]
+      this.mail = this.value[1]
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mymailshow {
+  background: #D9ECF2;
+  border: 5px solid black;
+  border-radius: 20px;
+  padding: 20px;
+}
+</style>
